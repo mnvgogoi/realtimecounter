08927b9e013ca80f589ee0e5a0ca3f81886a18c6
Vantage Circle <vantagecircle@Vantages-MacBook-Pro.local>
2022-09-22 10:50:48 +0530

    first commit

---
+++ RealtimeCounter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealtimeCounter
+//
+//  Created by Vantage Circle on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
